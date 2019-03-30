e64ea8c976ceb1c760176d937d9fe736e507faf4
aze-fortwo <azefortwo@protonmail.com>
2019-03-30 14:54:03 +0100

    Trying to fix closing multi opened folder
    When I try to close a folder in the Lbx, if some subFolder are open in it, lbx doesn't delete all of the content.
    
    The pbm come from is_open_in_listbox() for unknown reason

--- tk_listbox.py
+++ tk_listbox.py
@@ -6,67 +6,89 @@ import tk_init as tki
 
 def Listbox_click(event):
 	lbx_line = tki.Listbox.curselection()
-	content_clicked = tki.Listbox.get(lbx_line)
+	clicked_content = tki.Listbox.get(lbx_line)
 
-	print('\nLBX Clicked on "{}"'.format(content_clicked))
+	print('Lbx Clicked on "'+ clicked_content + '"')
 
-	if Folder.is_in_foldList(content_clicked):
-		folder_clicked = Folder.get_folder_in_foldList(content_clicked)
-		if is_open_in_Listbox(folder_clicked):
-			Listbox_delete_contentList(folder_clicked.contentList, lbx_line)
+
+	if Folder.is_in_foldList(clicked_content):
+
+		clicked_folder = Folder.get_folder_in_foldList(clicked_content)
+
+
+		if is_open_in_Listbox(clicked_folder):
+			Listbox_delete_contentList(clicked_folder.contentList, lbx_line)
+		
 		else:
-			Listbox_insert_contentList(folder_clicked.contentList, lbx_line)
+			Listbox_insert_contentList(clicked_folder.contentList, lbx_line)
+
+
+"""--------------------------Listbox_click method-------------------------"""
 
 
 def is_in_foldList(searched_folderName):
-	print('\n\tLBX Is "{}" in foldList ?'.format(searched_folderName))
+	#print('LBX  Is "{}" in foldList ?\n'.format(searched_folderName))
 	try:
 		if searched_folderName in Folder.foldList:
-			print('\t\tIt is in foldList')
+			#print('      It is in foldList')
 			return True
 		else:
-			print('\t\tNot in foldList')
+			#print('     Not in foldList')
 			return False
 
-	except:
-		print("\tSearching foldList for {} FAILED.\n".format(searched_folderName))
+	except Exception as exception:
+		print(exception)
+		print("\nSearching foldList for {} FAILED.\n".format(searched_folderName))
+
 
 
 def is_open_in_Listbox(folder):
-	print('\n\tLBX Is "{}" open in listbox ?'.format(folder.name))
+	print('\nLBX  Is "{}" open in listbox ?'.format(folder.lbx_name))
+	try:
+		contentList_content_detected_in_lbx = len(folder.contentList)
+
+
+		for content in folder.contentList:
+			if content.lbx_name in tki.Listbox.get(0,last=tki.tk.END):
+				contentList_content_detected_in_lbx -= 1
+
+
+		if contentList_content_detected_in_lbx == 0:
+			print('     It is.')
+			return True
+
+		else:
+			print("     It's not.")
+			return False
 	
-	contentList_len = len(folder.contentList)
-	for content in folder.contentList:
-		if content.name in tki.Listbox.get(0,last=tki.tk.END):
-			contentList_len -= 1
-			try:
-				if contentList_len == 0:
-					print('\t    It is')
-					return True
-					break
-			except:
-				return False
-	if content.name not in tki.Listbox.get(0,last=tki.tk.END):
-		print("\t    It's not ")
+	except Exception as exception:
+		print(exception)
+		print('\nIs "{}" open in Lbx FAILED.\n'.format(folder.name))
+
 
 
 def Listbox_delete_contentList(contentList, lbx_line):
-	print("\n\tLBX Delete {} item(s) from listbox.".format(len(contentList)))
-
+	#print("LBX  Delete {} item(s) from listbox.\n".format(len(contentList)))
 	try:
 		for content in contentList:
-			print('\t    Delete "{}" from listbox.'.format(content.name))
 			tki.Listbox.delete(lbx_line[0]+1)
-	except:
-		print("\t\tDelete {} items from listbox FAILED.\n".foamt(len(contentList)))
+			print('    Delete "{}" from listbox.'.format(content.name))
+
+	except Exception as exception:
+		print(exception)
+		print("Delete {} items from listbox FAILED.\n".format(len(contentList)))
+
+
 
 def Listbox_insert_contentList(contentList, lbx_line):
-	print("\n\tLBX Add {} item(s) to listbox :".format(len(contentList)))
+	#print("LBX  Add {} item(s) to listbox :".format(len(contentList)))
 	
 	try:
 		for content in contentList:
-			print('\t    Add "{}" to listbox.'.format(content.name))
-			tki.Listbox.insert(lbx_line[0]+1, content.name)
-	except:
-		print("Delete {} items from listbox FAILED.\n".foamt(len(contentList)))		
+			print('    Add "{}" to listbox.'.format(content.name))
+			tki.Listbox.insert(lbx_line[0]+1, content.lbx_name)
+
+	except Exception as exception:
+		print(exception)
+		print("\nAdd {} items from listbox FAILED.\n".format(type(exception).__name__, len(contentList)))		
 
